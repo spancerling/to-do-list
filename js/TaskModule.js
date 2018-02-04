07b056407220307eb1275e17762df5eff5224318
00 <1294401816@qq.com>
2018-02-04 13:37:50 +0800

    ES5

---
+++ js/TaskModule.js
@@ -0,0 +1,22 @@
+define(function(){
+	function TaskModule() {		
+	}
+	TaskModule.prototype = {
+		dele: function (element) {
+    	    var l = element.parentNode,
+                p = l.parentNode;
+	    	    p.removeChild(l);      
+        },
+        isFinished : function (element1,area) {
+    	    if(element1.checked == true){
+	    	    this.dele(element1);	
+	    	    var li = element1.parentNode;
+	    	    area.appendChild(li);
+	            li.class = "task_finished";
+	        }
+	    }
+    }
+    return {
+                TaskModule:TaskModule
+	}
+})
