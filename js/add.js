d01ca73a9c7302547035ac3a36143c1cb0f1044b
spancerling <1294401816@qq.com>
2018-02-10 12:12:47 +0800

    commit

--- js/add.js
+++ js/add.js
@@ -1,17 +1,17 @@
-//声明全局变量将每个todo对象保存在数组里
-var TODOLIST=[];
+
+var todolist=[];
 var listModule = {
         add:function (){
             var content = document.getElementById("list_type");
             var obj_list ={
-    	        todo: "",/*保存内容*/
+    	        todo: "",
     	        done: false
             };
             if(content.value){
                 var content = document.getElementById("list_type");
                 obj_list.todo = content.value;
-                TODOLIST.push(obj_list);
-                dataModule.saveData(TODOLIST);
+                todolist.push(obj_list);
+                dataModule.saveData(todolist);
                 content.value="";
                 show();
             }
@@ -21,33 +21,22 @@ var listModule = {
             localStorage.clear();  
             show();  
         },
-        /**
-         * [switchStyle]
-         * @param  {[string]} target [应用样式的元素的id]
-         * @param  {[string]} style  [class的名字]
-         */
         switchStyle : function(target,style) {
-		   var box = document.getElementById(target);
-		   box.classList.add(style);
-	    },
-	    removeStyle : function(target,style) {
-		   var box = document.getElementById(target);
-		   box.classList.remove(style);
-	    }  
+		var box = document.getElementById(target);
+		box.classList.add(style);
+	},
+	removeStyle : function(target,style) {
+		var box = document.getElementById(target);
+		box.classList.remove(style);
+	}  
 	}
-var dataModule = {
-	    /**
-	     * [saveData 把对象转换为json字符串储存在localStorage]
-	     * @param  {[object]} data 
-	     * @return {[string]}      
-	     */
+var dataModule = {			
 	    saveData: function(data){
             localStorage.setItem("mytodolist",JSON.stringify(data));
 	    },
 	    loadData: function(){
             var hisTory = localStorage.getItem("mytodolist");
 	        if(hisTory!==null){
-	        	//将json字符串转化为对象
 		        return JSON.parse(hisTory);
 	        }
 	        else{
@@ -56,40 +45,37 @@ var dataModule = {
 	    }
 	}
 function show() {
-    var    tasks = document.getElementById("list_tasks"),
+    var  tasks = document.getElementById("list_tasks"),
     finishedArea = document.getElementById("list_finished"),
-      todoString = "",
-      doneString = "";
-    TODOLIST = dataModule.loadData();
-    //从localStorage中获取并渲染呈现，用html一次赋值避免重复dom操作
-    for(var i=0;i<TODOLIST.length;i++) {
-    	//通过done属性在不同区域呈现
-    	if(!TODOLIST[i].done){
+    todoString = "",
+    doneString = "";
+    todolist = dataModule.loadData();
+    for(var i=0;i<todolist.length;i++) {
+    	if(!todolist[i].done){
     		todoString +="<li>"+"<input type='checkbox' onchange='update("+i+",\"done\", true)'>"
-    		+TODOLIST[i].todo+"<button onclick='remove("+i+")'>DEL</button>"+"</li>";
+    		+todolist[i].todo+"<button onclick='remove("+i+")'>DEL</button>"+"</li>";
     	}
     	else{
     		doneString +="<li>"+"<input type='checkbox' onchange='update("+i+",\"done\", false)' checked>"
-    		+TODOLIST[i].todo+"<button onclick='remove("+i+")'>DEL</button>"+"</li>";
+    		+todolist[i].todo+"<button onclick='remove("+i+")'>DEL</button>"+"</li>";
     	}
     }
     tasks.innerHTML = todoString;
     finishedArea.innerHTML = doneString;
 }
-//改变done的布尔值
 function update(i,field,value) {
-	TODOLIST[i][field] =value;
-	dataModule.saveData(TODOLIST);
+	todolist[i][field] =value;
+	dataModule.saveData(todolist);
 	show();
 }
 function remove(i) {
-	TODOLIST.splice(i,1);
-	dataModule.saveData(TODOLIST);
+	todolist.splice(i,1);
+	dataModule.saveData(todolist);
 	show();
 }
 window.addEventListener("load",show());
 document.getElementById("add").onclick = function(){
-	listModule.add(); 
+	listModule.add();
 }
 document.getElementById("list_type").onkeydown = function(event){
 	if(event.keyCode == 13){
--- vue/add.js
+++ vue/add.js
@@ -1,25 +1,25 @@
 
 var vm = new Vue({
-  el:"#vue-todolist",
-  data:{
-      inputValue:"",
-      items:[],
-      index:0,
-      isActive:false
-  },
-  methods:{
-    add:function(){
-        if(this.inputValue){
-         this.items.push({
-          text: this.inputValue,
-          id: parseInt(this.index)+1
-         })
-         this.inputValue=""
-       }
+    el:"#vue-todolist",
+    data:{
+        inputValue:"",
+        items:[],
+        index:0,
+        isActive:false
     },
-    change:function(){
-     this.isActive = !(this.isActive)
+    methods:{
+        add:function(){
+            if(this.inputValue){
+                this.items.push({
+                text: this.inputValue,
+                id: parseInt(this.index)+1
+                })
+                this.inputValue=""
+            }
+        },
+        change:function(){
+            this.isActive = !(this.isActive)
+        }
     }
-  }
 })
 
